f02045b1b79b40d6be8e57014a88a96e2bad4e3f
Django Datama <django@datama.fr>
2020-12-17 14:29:00 +0100

    Debug for file location

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 echo "\e[33m\e[1mR session information"
 Rscript -e 'sessionInfo()'
 
+pwd
+
 # Check for build only
 if [ "$1" = "build" ]; then
     echo "\e[33m\e[1mRunning only build task"
     R CMD INSTALL . -c --build
 fi
+
+ls
