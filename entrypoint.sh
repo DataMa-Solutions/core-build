f998ac670e6617e807f2042a741d83e349b11224
Django Unchained <django@datama.fr>
2020-12-17 14:44:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
+#!/bin/sh -l
+
 echo "\e[33m\e[1mR session information"
 Rscript -e 'sessionInfo()'
-
+echo "\e[33m\e[1mR Location"
 pwd
 
 # Check for build only
@@ -8,5 +10,5 @@ if [ "$1" = "build" ]; then
     echo "\e[33m\e[1mRunning only build task"
     R CMD INSTALL . -c --build
 fi
-
+echo "\e[33m\e[1mR Files generated"
 ls
